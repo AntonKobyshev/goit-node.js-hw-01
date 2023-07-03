fe42f269b37ce8d867623456f204b235c7b3cfd8
AntonKobyshev <antonkobyshev@gmail.com>
2023-07-03 17:56:14 +0300

    updt

--- contacts.js
+++ contacts.js
@@ -1,6 +1,34 @@
 const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+console.log(contactsPath)
+
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath )
+    return JSON.parse(data);
+}
 
 
+const getContactById = async (contactId) => {
+    const contacts = await listContacts();
+    const contact = contacts.find((contact) => contact.id === contactId)
+    return contact || null;
 
+}
 
-console.log();
+// const removeContact = async (contactId) => {
+
+// }
+
+// const addContact= async (name, email, phone) => {
+
+// }
+
+module.exports = {
+    listContacts,
+    getContactById,
+    // removeContact,
+    // addContact
+}
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("welcome")
+const contacts = require("./contacts");
+
+// const argv = require('yargs').argv;
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      return console.log(allContacts);
+      break;
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+      break;
+    case "add":
+      break;
+
+    // case 'remove':
+    //   // ... id
+    //   break;
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+// invokeAction(argv);
+
+invokeAction({
+  action: "add",
+  name: "asd",
+  email: "dui.in@egetlacus.ca",
+  phone: "(294) 840-6685",
+});
